7c0bb855c868211ea3b691c8eae804d07801edb6
priyanshu6091 <145902260+priyanshu6091@users.noreply.github.com>
2024-06-11 17:23:22 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://library-management-system-six-mu.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 // Routes
