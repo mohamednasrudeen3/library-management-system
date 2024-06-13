f531e55736af64ee3649b68f1ed81ac6edde2f07
priyanshu6091 <145902260+priyanshu6091@users.noreply.github.com>
2024-06-13 17:27:34 +0530

    Update Login.js

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('/api/auth/login', { email, password });
+      const res = await axios.post('https://library-management-system-mern-faac.onrender.com/api/auth/login', { email, password });
       localStorage.setItem('token', res.data.token);
       console.log("login",res.data.token)
       navigate('/dashboard');
