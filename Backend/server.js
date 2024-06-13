cc829eb31690a3a8e5a0330669747ba5c363d790
priyanshu6091 <145902260+priyanshu6091@users.noreply.github.com>
2024-06-13 17:08:15 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://main--fastidious-pony-356a26.netlify.app/',
+  origin: 'https://666ad8407e87bb000858564b--lustrous-kitten-c7214d.netlify.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials:true,
 }));
