8f5241c131ec9227784d093862426b4b6b7cf06b
priyanshu6091 <145902260+priyanshu6091@users.noreply.github.com>
2024-06-14 12:55:29 +0530

    Update AddBook.js

--- Frontend/src/pages/AddBook.js
+++ Frontend/src/pages/AddBook.js
@@ -11,7 +11,7 @@ const AddBookForm = ({ onBookAdded }) => {
     e.preventDefault();
     try {
       // Use full API URL if backend is on a different server
-      await axios.post(`https://main--lustrous-kitten-c7214d.netlify.app/api/books`, { title, author, category, stock });
+      await axios.post(`https://library-management-system-mern-faac.onrender.com/api/books`, { title, author, category, stock });
       onBookAdded();
       setTitle('');
       setAuthor('');
